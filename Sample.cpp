13af742e1a3de052b1b0da90bab09309e11dd210
JAISURYAH K P <118588201+jaisuryah@users.noreply.github.com>
2024-10-15 15:02:26 +0530

    Update Sample.cpp

--- Sample.cpp
+++ Sample.cpp
@@ -1,88 +1,87 @@
-// DAY 1
 #include <iostream>
 using namespace std;
 
-// void swap(int *a,int *b)
-// {
-//     int temp = *a;
-//     *a=*b;
-//     *b=temp;
-// }
+void swap(int *a,int *b)
+{
+    int temp = *a;
+    *a=*b;
+    *b=temp;
+}
 
-// int findmax(int *arr,int n)
-// {
-//     int max=*arr;
-//     for(int i=1;i<n;i++)
-//     {
-//         if(*(arr+i)>max)
-//         {
-//             max=*(arr+i);
-//         }
-//     }
+int findmax(int *arr,int n)
+{
+    int max=*arr;
+    for(int i=1;i<n;i++)
+    {
+        if(*(arr+i)>max)
+        {
+            max=*(arr+i);
+        }
+    }
     
-//     return max;
-// }
+    return max;
+}
 
-// int findsum(int *arr,int n)
-// {
-//     int sum=0;
-//     for(int i=0;i<n;i++)
-//     {
-//         sum+=*(arr+i);
-//     }
-//     return sum;
-// }
+int findsum(int *arr,int n)
+{
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=*(arr+i);
+    }
+    return sum;
+}
 
-// int findavg(int *arr,int n)
-// {
-//     int sum=0;
-//     for(int i=0;i<n;i++)
-//     {
-//         sum+=*(arr+i);
-//     }
-//     return sum/n;
-// }
+int findavg(int *arr,int n)
+{
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=*(arr+i);
+    }
+    return sum/n;
+}
 
-// void reverse(char *a,int n)
-// {
-//     char *end=a;
-//     while(*end)
-//     {
-//         end++;
-//     }
-//     end--;
+void reverse(char *a,int n)
+{
+    char *end=a;
+    while(*end)
+    {
+        end++;
+    }
+    end--;
     
-//     while(a<end)
-//     {
-//         char temp=*a;
-//         *a=*end;
-//         *end = temp;
-//         a++;
-//         end--;
-//     }
-// }
+    while(a<end)
+    {
+        char temp=*a;
+        *a=*end;
+        *end = temp;
+        a++;
+        end--;
+    }
+}
 
-// void copy(char *d,char *a)
-// {
-//     while(*a!='\0'){
-//         *d=*a;
-//         a++;
-//         d++;
-//     }
-//     *d='\0';
-// }
+void copy(char *d,char *a)
+{
+    while(*a!='\0'){
+        *d=*a;
+        a++;
+        d++;
+    }
+    *d='\0';
+}
 
-// bool search(char *a,char target,int n)
-// {
-//     for(int i=0;i<n;i++)
-//     {
-//         if(*(a+i)==target)
-//         {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
+bool search(char *a,char target,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        if(*(a+i)==target)
+        {
+            return true;
+        }
+    }
+    return false;
+}
 
 bool search(int *a,int target,int n)
 {
@@ -94,6 +93,30 @@ bool search(int *a,int target,int n)
         }
     }
     return false;
+}
+
+int factorial(int n)
+{
+
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
+int fibba(int n)
+{
+    if(n==0 || n==1)
+    {
+        return n;
+    }
+    else
+    {
+        return fibba(n-1)+fibba(n-2);
+    }
+        
+    
 }
 
 
@@ -144,15 +167,45 @@ int main() {
     // copy(d,a);
     // cout<<d;
     
-    int target=3;
-    int n = sizeof(arr)/sizeof(arr[0]);
-    if(search(arr,target,n)){
-        cout<<"present";
-    }
-    else
+    // int target=3;
+    // int n = sizeof(arr)/sizeof(arr[0]);
+    // if(search(arr,target,n)){
+    //     cout<<"present";
+    // }
+    // else
+    // {
+    //     cout<<"absent";
+    // }
+    
+    // int n;
+    // cin>>n;
+    // int fact=1;
+    // for(int i=1;i<=n;i++)
+    // {
+    //     fact*=i;
+    // }
+    // cout<<fact<<endl;
+    // cout<<factorial(n);
+    
+//     int n,i=0;
+//     cin>>n;
+//     while(i < n) {
+//   cout << " " << fibba(i);
+//   i++;
+// }
+
+    char str[] = "hello";
+    int vowels;
+    for(int i = 0; str[i]; i++)  
     {
-        cout<<"absent";
+        if(str[i]=='a'|| str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'
+        ||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O' ||str[i]=='U')
+        {
+		    vowels++;
+        }
     }
+    cout<<vowels;
+    
     
     
     
