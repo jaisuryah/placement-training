109d68c976ee3ab484f75125f869b806bbd8f59f
JAISURYAH K P <118588201+jaisuryah@users.noreply.github.com>
2024-10-16 12:10:19 +0530

    Update Conditional&looping.java

--- Conditional&looping.java
+++ Conditional&looping.java
@@ -64,8 +64,8 @@ class HelloWorld {
         //     System.out.println("No real roots");
         // }
         
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        // int n = sc.nextInt();
+        // int m = sc.nextInt();
         
         // for(int i=0;i<n;i++)
         // {
@@ -88,14 +88,102 @@ class HelloWorld {
         //         System.out.println();
         // }
         
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                System.out.print();
-            }
-                System.out.println();
-        }
+        
+        //left angled triangle
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        //     }
+
+        
+        //right angled triangle
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = n; j > i; j--) {
+        //         System.out.print("  ");
+        //     }
+        //     for (int k = 1; k <= i; k++) {
+        //         System.out.print("* ");
+        //     }
+        // System.out.println();
+        // }
+        
+        // inverted left angled triangle
+        // int n = 5;
+        // for (int i = n; i >= 1; i--) {
+        //     for (int j = 1; j <= i; j++) {
+        //          System.out.print("* ");
+        //         }
+        //     System.out.println();
+        //     }   
+        
+        //inverted right angled triangle
+        // int n = 5;
+        // for (int i = n; i >= 1; i--) {
+        //     for (int j = n; j > i; j--) {
+        //         System.out.print("  ");
+        //         }
+        //     for (int k = 1; k <= i; k++) {
+        //         System.out.print("* ");
+        //         }
+        //      System.out.println();
+        //     }
+        
+        
+        //pyramid
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = n; j > i; j--) {
+        //         System.out.print(" ");
+        //         }
+        //     for (int k = 1; k <= (2 * i - 1); k++) {
+        //          System.out.print("*");
+        //         }
+        //      System.out.println();
+        // }
+        
+        //inverted pyramid
+            //  int n = 5;
+            // for (int i = n; i >= 1; i--) {
+            //      for (int j = n; j > i; j--) {
+            //         System.out.print(" ");
+            //         }
+            //      for (int k = 1; k <= (2 * i - 1); k++) {
+            //         System.out.print("*");
+            //         }
+            //         System.out.println();
+            // }
+          
+          //diamond  
+// int n = 5;
+// for (int i = 1; i <= n; i++) {
+//     for (int j = n; j > i; j--) {
+//         System.out.print(" ");
+//     }
+//     for (int k = 1; k <= (2 * i - 1); k++) {
+//         System.out.print("*");
+//     }
+//     System.out.println();
+// }
+// for (int i = n - 1; i >= 1; i--) {
+//     for (int j = n; j > i; j--) {
+//         System.out.print(" ");
+//     }
+//     for (int k = 1; k <= (2 * i - 1); k++) {
+//         System.out.print("*");
+//     }
+//     System.out.println();
+// }
+
+
+        
+
+
+
+
         
         
         
