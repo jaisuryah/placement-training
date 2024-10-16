622956cc3303fd19f0d31ed1c61dfe38ce266659
JAISURYAH K P <118588201+jaisuryah@users.noreply.github.com>
2024-10-16 11:01:33 +0530

    Create Conditional&looping.java

---
+++ Conditional&looping.java
@@ -0,0 +1,105 @@
+import java.util.*;
+import java.lang.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int mark = sc.nextInt();
+        // if(mark>=91 && mark<=100)
+        //     System.out.println("O");
+        // else if(mark>=81 && mark<=90)
+        //     System.out.println("A");
+        // else if(mark>=71 && mark<=80)
+        //     System.out.println("B");
+        // else if(mark>=61 && mark<=70)
+        //     System.out.println("C");
+        // else 
+        //     System.out.println("F");
+        
+        
+        // int yr = sc.nextInt();
+        // if((yr%4==0)&&!(yr%100==0)||(yr%400==0))
+        // {
+        //      System.out.println("leap year");
+        // }
+        // else
+        // {
+        //      System.out.println("not leap year");
+        // }
+        
+        // int r = sc.nextInt();
+        // int c = sc.nextInt();
+        // int tree = sc.nextInt()
+        // int a[][] = new int[r][c];
+        // for(int i=0;i<r;i++)
+        // {
+        //     for(int j=0;j<c;j++)
+        //     {
+        //         a[i][j] = sc.nextInt();
+        //     }
+        // }
+        //         if((tree>=1 && tree<=c) || (tree%c==1) || (tree%4==0))
+        //         {
+        //             System.out.println("mango tree");
+        //         }
+        //         else
+        //         {
+        //             System.out.println("apple tree");
+        //         }
+        
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        
+        // double del = Math.pow(b,2) - 4*(a*c);
+        
+        // if(del>0)
+        // {
+        // double root = (-b + Math.sqrt(del))/(2*a);
+        // double root1 = (-b - Math.sqrt(del))/(2*a);
+        // System.out.println(del+" "+root+" "+root1);
+        // }
+        // else
+        // {
+        //     System.out.println("No real roots");
+        // }
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<m;j++)
+        //     {
+        //         System.out.print("*");
+        //     }
+        //         System.out.println();
+        // }
+        
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<m;j++)
+        //     {
+        //         if(i==0 || i==n-1 || j==0 || j==m-1)
+        //         System.out.print("*");
+        //         else
+        //         System.out.print(" ");
+        //     }
+        //         System.out.println();
+        // }
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                System.out.print();
+            }
+                System.out.println();
+        }
+        
+        
+        
+        
+        
+    }
+}
